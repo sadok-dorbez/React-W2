bd6d55ec1aa4a9a597b115aed758aa0d03b6ce0c
sadok-dorbez <sadokdorbez0@gmail.com>
2023-04-11 21:40:04 +0100

    V3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import Products from './comoosants/Products'
+import React, { Suspense, lazy } from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+
+//import Header from './components/Header';
+//import Loading from './components/Loading';
+
+//const Home = lazy(() => import('./pages/Home'));
+//const About = lazy(() => import('./pages/About'));
+const Products = lazy(() => import('./comoosants/Product'));
+
 function App() {
   return (
-    <div className="App">
-      <Products />
-    </div>
+    /*<Router>
+      <Header />
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </Suspense>
+  </Router>*/
+
+  <Router>
+    <Switch>
+      <Route path="/products" component={Products}/>
+      
+    </Switch>
+    </Router>
   );
 }
 
