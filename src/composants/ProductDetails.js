badb147f617567e28373186b672d2c3272f4c83a
sadok-dorbez <sadokdorbez0@gmail.com>
2023-04-11 23:11:26 +0100

    V5

--- src/composants/ProductDetails.js
+++ src/composants/ProductDetails.js
@@ -1,32 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
 import { getProductById } from "../services/api";
 
-const ProductDetails = () => {
-  const { id } = useParams();
-  const [product, setProduct] = useState([]);
+const ProductDetails = ({ match }) => {
+  const [product, setProduct] = useState(null);
+  const { id } = match.params;
 
   useEffect(() => {
-    getProductById(id).then((data) => {
-        console.log(data)
-        setProduct(data);
-    });
+    const fetchProduct = async () => {
+      const data = await getProductById(id);
+      setProduct(data);
+    };
 
-}, []);
+    fetchProduct();
+  }, [id]); // add id to the dependency array
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      {product ? (
-        <div className="product-details">
-          <h2>{product.name}</h2>
-          <p>{product.description}</p>
-          <p>Price: {product.price}$</p>
-          <p>Quantity: {product.quantity}</p>
-          <img src={product.image} alt={product.name} />
-        </div>
-      ) : (
-        <p>Product not found.</p>
-      )}
+      <h2>{product.name}</h2>
+      <p>Price: {product.price}</p>
+      <p>Description: {product.description}</p>
+      <img src={product.image} alt={product.name} />
     </div>
   );
 };
